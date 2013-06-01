111ba05ad9576eb9c44a45a8a8ccc35a6d209eb7
AndresLo <grimeyyyy@googlemail.com>
2013-06-01 19:29:56 +0200

    php sql template

--- WebTech/WebContent/templates/create_table.php
+++ WebTech/WebContent/templates/create_table.php
@@ -1,3 +1,24 @@
+<-- legt Datenbank und die Tabelle an --></-->
+
 <?php
-    phpinfo();
-?>
+
+	//Datenbank anlegen
+    $con = mysql_connect("localhost", "peter", "abc123");
+	if (mysql_query("CREATE DATABASE my_db", $con))
+		{
+		echo "Database created";
+		}
+	else
+		{
+		echo "Error creating database: " . mysql_error();
+		}
+		
+	//Tabelle anlegen
+	mysql_select_db("my_db", $con);
+	$sql = "CREATE Table Wetterinfos (Winddirection varchar(15),
+									Windstrength varchar(15))";
+
+	//Execute query
+	mysql_query($sql,$con);
+		
+?>		
